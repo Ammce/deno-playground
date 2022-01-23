da663dd9d12c9d304352ed9731b36ab05df65059
Amel Muminovic <amcenp@gmail.com>
2022-01-23 15:22:03 +0100

    Writing to file with deno

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Hello from Deno");
+const text = "Hello from Deno";
+
+const encoder = new TextEncoder();
+
+const encodedText = encoder.encode(text);
+
+Deno.writeFile("sample.txt", encodedText).then((data) => {
+  console.log(data);
+});
